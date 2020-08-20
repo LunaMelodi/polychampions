04077c6662bc339e41076a66ba3633831383bdf1
Lindsey <lunacadwell2@gmail.com>
2020-08-20 01:11:11 -0400

    Intial team content page template created

--- scripts/page_generation/team.js
+++ scripts/page_generation/team.js
@@ -7,10 +7,34 @@ export default function generateTeamPage(teamData) {
   let teamContentContainer = document.createElement('div');
   
   let teamNameHeading = document.createElement('h1');
-  teamNameHeading.innerText = teamData.proName;
+  teamNameHeading.innerText = teamData.proName +' and '+ teamData.minorName;
   teamContentContainer.appendChild(teamNameHeading);
   
   let aboutHeading = document.createElement('h3');
+  aboutHeading.innerText = 'About '+ teamData.proName +':';
+  teamContentContainer.appendChild(aboutHeading);
+
+  let aboutSection = document.createElement('p');
+  aboutSection.innerText = teamData.about;
+  teamContentContainer.append(aboutSection);
+
+  let historyHeading = document.createElement('h3');
+  historyHeading.innerText = 'Team History:';
+  teamContentContainer.append(historyHeading);
+
+  let historySection = document.createElement('p');
+  historySection.innerText = teamData.history;
+  teamContentContainer.append(historySection);
+
+  if(teamData.trophies.length > 0) {
+    let trophyHeader = document.createElement('h3');
+    trophyHeader.innerText = 'Trophies:'
+    teamContentContainer.append(trophyHeader);
+    
+    let trophySection = document.createElement('p');
+    trophySection.innerText = teamData.trophies.length > 1 ? teamData.trophies.join(', ') : teamData.trophies[0];
+    teamContentContainer.append(trophySection);
+  }
 
   let teamContentPlaceholder = document.querySelector('#teamContentPlaceholder');
   teamContentPlaceholder.appendChild(teamContentContainer);
--- team_content/cosmonauts.js
+++ team_content/cosmonauts.js
@@ -3,5 +3,5 @@ export let teamInfo = {
   minorName: "The Space Cadets",
   about: "great team 10/10",
   history: "we were there, and now we are here",
-  trophies: ["s5junior"]
+  trophies: []
 };
