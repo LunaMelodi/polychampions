c0cc1962eb389ae0752c9156a00955b4d7ed08e9
Artemis21 <hollyshort21@gmail.com>
2020-08-24 20:17:49 +0100

    Moved generated files to seperate folder.

--- client/src/main.js
+++ client/src/main.js
@@ -1,14 +1,32 @@
 var nunjucks = require('nunjucks');
 var fs = require('fs');
-var teams = require('../data/teamsData.js')
+var teams = require('./data/teamsData.js');
+
+
+function makeFolder(path) {
+    if (!fs.existsSync(path)){
+        fs.mkdirSync(path);
+    }
+}
+
+
+function transferFolder(folder) {
+    makeFolder('dist/' + folder);
+    var files = fs.readdirSync('src/' + folder);
+    for (file of files) {
+        file = folder + '/' + file;
+        fs.copyFileSync('src/' + file, 'dist/' + file);
+    }
+}
 
 
 function render(template, target, context) {
     var html = nunjucks.render('src/templates/' + template + '.njk', context);
-    fs.writeFileSync('src/' + target + '.html', html)
+    fs.writeFileSync('dist/' + target + '.html', html)
 }
 
 function renderFolder(folder) {
+    makeFolder('dist/' + folder);
     var files = fs.readdirSync('src/view-content/' + folder);
     for (file of files) {
         var title = file.slice(0, file.length - 5);
@@ -17,9 +35,12 @@ function renderFolder(folder) {
     }
 }
 
+makeFolder('dist');
+makeFolder('dist/teams');
 for (team of teams.allTeams) {
     render('team', 'teams/' + team.shortName, { team: team });
 }
 render('index', 'index', {});
-renderFolder('stats')
-renderFolder('guides')
+renderFolder('stats');
+renderFolder('guides');
+transferFolder('js');
