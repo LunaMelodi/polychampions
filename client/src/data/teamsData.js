a3482412fb6d3819845742c0ccd470e2b1bb0a1f
Artemis <hollyshort21@gmail.com>
2021-01-28 20:19:56 +0000

    Add history/about for a few teams.
    Mostly not my work.

--- client/src/data/teamsData.js
+++ client/src/data/teamsData.js
@@ -27,8 +27,10 @@ module.exports.crawfish = {
   proName: "The Crawfish",
   minorName: "The Shrimps",
   slogan: "Who doesn't like being hard and wet...",
-  about: "",
-  history: "",
+  about: "Dedicated players with raw talent and a huge love for the game. Outside of league games they work their grey matter with competitive internal games and chess, kick back with fun badtribes games and chess, and sometimes chat about anime or chess. Half students, half dads, all claw!",
+  history: `On August 8th, 2018 Crawfish became the 10th team in the Major league after a multi-round server vote on possible teams. It was founded by Hughmungus and “Koric 1v2”, who quickly expanded the team with a vast array of talent. After winning 2 consecutive seasons 17-1 and losing only a single playoff game between the seasons, many of Crawfish's founding members retired or took a step back from the game.
+
+“theShow” became the second leader through an election, cementing Craw's democratic precedent. Crawfish continued to be stripped of its veterans through season 5 and 6 - during which Jprawnz and then Vorce lead the team - but going into s7 their new talent matured to become veterans in their own right, and some older players returned or became more active. With a powerful season 7 performance that gained them second seed going into the playoffs, and their junior Shrimps becoming the dominant minor team, Crawfish is poised to be a perennial top team.`,
   trophies: [],
   bgColor: "#FA8072",
   fgColor: "#000000"
@@ -39,7 +41,7 @@ module.exports.dragons = {
   proName: "The Dragons",
   minorName: "The Narwhals",
   slogan: "🔥🔥🔥🔥",
-  about: "",
+  about: "Being the second newest team besides Jalapeños, they have had a rough start to their 2 seasons. However, the team has great leadership and team spirit, and is far more relaxed than the Jets. They compete in a friendly environment, helping each other grow with friendly 2v2s and 1v1s. They hope to succeed and hopefully compete for the Season 8 championships, both minor and major. The Dragons are welcome to communicative players who are willing to learn.",
   history: "",
   trophies: [],
   bgColor: "#F74AB5",
@@ -76,7 +78,7 @@ module.exports.lightning = {
   minorName: "The Pulse",
   slogan: "We are a nicely run, family business.",
   about: "",
-  history: "",
+  history: `Created at the start of season 1, Lightning had a rough start. Starlord led the team through the first season, before handing the role to Scott. Scott wasn’t keen on being the leader but there were no other good candidates so he kept it. The team did badly until season 4 when Bakalol, Ben and Chief joined the team from Ronin. Under Bakalol they did much better, finishing no lower than fifth place for the 4 seasons he led them. At the end of season 7, Bakalol stepped down and gave leadership to VikingTheCat, who will hopefully lead Lightning to a glorious future.`,
   trophies: [],
   bgColor: "#C4C41B",
   fgColor: "#000000"
@@ -124,7 +126,10 @@ module.exports.sparkies = {
   minorName: "The Pups",
   slogan: "Our enemies will go blind from overexposure to pure awesomeness!",
   about: "",
-  history: "",
+  history: `One of the first few teams, Sparkies was created before the season 1. Sparkies had a successful first and second season (winning the Playoffs in season 1) before falling badly during season 3. However, Sparkies was rebuilt and rebranded in season 4, and since then they’ve been better than ever, with many of the top players.
+
+The Pups were created in season 5 as PolyChampions minor league expansion to allow more players to join teams. The junior team started off with a decent rating and continued to do well throughout season 6.
+`,
   trophies: [],
   bgColor: "#854D21",
   fgColor: "#FFFFFF"
