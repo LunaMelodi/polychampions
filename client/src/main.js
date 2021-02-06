d685f3e96c3197c716c205b5988c47f13a75c9fa
Artemis21 <artemisdev1@gmail.com>
2021-02-06 15:46:18 +0000

    Switch to a more casual dark theme.

--- client/src/main.js
+++ client/src/main.js
@@ -5,6 +5,7 @@ const teams = require('./data/teamsData.js');
 const PLACEHOLDER = `There's nothing here yet. If you have something to write,
  <a href="https://github.com/lunamelodi/polychampions">contribute on GitHub</a>
  or <a href="https://artemisdev.xyz">contact Artemis</a>.`;
+const HOST = 'https://beta.polytopia.win';
 
 function makeFolder(path) {
     if (!fs.existsSync(path)) {
@@ -26,6 +27,8 @@ function transferFolder(folder) {
 }
 
 function render(template, target, context) {
+    context.url = `${HOST}/${target}`;
+    context.host = HOST;
     const html = nunjucks.render('src/templates/' + template + '.njk', context);
     fs.writeFileSync('dist/' + target + '.html', html)
 }
@@ -48,8 +51,11 @@ for (var team of teams.allTeams) {
     render('team', 'teams/' + team.shortName, { team: team });
 }
 
-render('index', 'index', {});
+for (const page of [ 'index', 'about', '404' ]) {
+    render(page, page, {});
+}
 render('team_index', 'teams/index', { teams: teams.allTeams });
-renderFolder('stats', 'stats');
-renderFolder('guides', 'root');
+// No content here yet, templates need updating/creating when there is.
+// renderFolder('stats', 'stats');
+// renderFolder('guides', 'root');
 transferFolder('js');
